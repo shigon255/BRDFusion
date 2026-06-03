dc9c75ca75471b3cffca0ecc56e8b5a60ec244cf
Yi-Ruei Liu <88969068+shigon255@users.noreply.github.com>
2026-06-03 11:07:05 +0800

    Rename variables for clarity in stage_ckpt.sh

--- scripts/stage_ckpt.sh
+++ scripts/stage_ckpt.sh
@@ -1,15 +1,15 @@
 # Assume that ckpt lives in project_root/ckpt
 # Comment out the ckpt you haven't downloaded yet
 
-waymo_ckpts_to_stage=(
-    "003"
-    "019"
+waymo_scene_ids_to_stage=(
+    "3"
+    "19"
     "114"
     "172"
     "703"
 )
 
-self_ckpts_to_stage=(
+self_path_ids_to_stage=(
     "1"
     "2"
     "3"
@@ -18,26 +18,27 @@ self_ckpts_to_stage=(
     "6"
 )
 
-for waymo_ckpt in "${waymo_ckpts_to_stage[@]}"; do
+for waymo_scene_id in "${waymo_scene_ids_to_stage[@]}"; do
+    scene3dx=$(printf "%03d" ${waymo_scene_id})
     # All waymo ckpts are trained under 1cam and 51 frames, with test_image_stride = 10.
     conda run -n brdfusion --no-capture-output python -u tools/stage_render_checkpoint.py \
-        --checkpoint ./ckpt/waymo/${waymo_ckpt}/checkpoint_final.pth \
+        --checkpoint ./ckpt/waymo/${scene3dx}/checkpoint_final.pth \
         --dataset waymo \
         --cams 1 \
-        --scene_idx ${waymo_ckpt} \
+        --scene_idx ${waymo_scene_id} \
         --num_frames 51 \
         --data_root ./data/waymo/processed/training \
         --output_root ./work_dirs \
         --symlink
 done
 
-for self_ckpt in "${self_ckpts_to_stage[@]}"; do
+for self_path_id in "${self_path_ids_to_stage[@]}"; do
     # All self ckpts are trained under 1cam and 51 frames, with test_image_stride = 10.
     conda run -n brdfusion --no-capture-output python -u tools/stage_render_checkpoint.py \
-        --checkpoint ./ckpt/self/path${self_ckpt}_fixed_tree_gamma_full/qwantani_moon_noon_puresky_4k/checkpoint_final.pth \
+        --checkpoint ./ckpt/self/path${self_path_id}_fixed_tree_gamma_full/qwantani_moon_noon_puresky_4k/checkpoint_final.pth \
         --dataset self \
         --cams 1 \
-        --path_id ${self_ckpt} \
+        --path_id ${self_path_id} \
         --scene_idx qwantani_moon_noon_puresky_4k \
         --num_frames 51 \
         --symlink
